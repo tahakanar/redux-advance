89ec72aedc8dddb8d6e14987527c97934c9e08d2
tahakanar <tahakanar@gmail.com>
2022-01-10 00:20:26 +0300

    redux practice project

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,43 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My first book",
+    description: "The first book I ever wrote",
+  },
+  {
+    id: "p2",
+    price: 3,
+    title: "My first Macbook",
+    description: "The first Macbook I ever bought",
+  },
+  {
+    id: "p3",
+    price: 8,
+    title: "My last glass",
+    description: "The first book I ever wrote",
+  },
+];
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
